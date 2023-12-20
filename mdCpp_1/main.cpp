fbaf8b05bfc4530d1e64fc0158677d6b2e4bb362
CocoPig1028 <eodud6411@gmail.com>
2023-12-21 01:24:14 +0900

    reference note

--- mdCpp_1/main.cpp
+++ mdCpp_1/main.cpp
@@ -6,9 +6,18 @@
 //
 
 #include <iostream>
+using namespace std;
 
-int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+int change_val(int *p) {
+    *p = 3;
+    
     return 0;
+}
+
+int main() {
+    int number = 5;
+    
+    cout << number << endl;
+    change_val(&number);
+    cout << number << endl;
 }

1f93d966da41f57c73be2819b833d97acdc6d0d0
CocoPig1028 <eodud6411@gmail.com>
2023-12-21 23:51:47 +0900

    modoo coding Cpp

---
+++ mdCpp_4/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  mdCpp_4
+//
+//  Created by 박대영 on 12/21/23.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
